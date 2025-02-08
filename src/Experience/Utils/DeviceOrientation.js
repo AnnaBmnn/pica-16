ffc72ddab26bc6979ae520040341d7964c0974c2
Anna <anna@akufen.ca>
2025-02-07 19:11:35 -0500

    add permission

--- src/Experience/Utils/DeviceOrientation.js
+++ src/Experience/Utils/DeviceOrientation.js
@@ -7,11 +7,18 @@ export default class DeviceOrientation extends EventEmitter
         super()
 
         console.log('HEY')
-        window.addEventListener("devicemotion", this.handleMotionEvent, true);
+        DeviceMotionEvent.requestPermission()
+        .then(response => {
+          if (response == 'granted') {
+            window.addEventListener("devicemotion", this.handleMotionEvent, true);
+
+          }
+        })
+        .catch(console.error)
     }
     handleMotionEvent(event) {
         console.log(event)
-        document.querySelector('.js-title').innerHTML = event
+        document.querySelector('.js-title').innerHTML = event.accelerationIncludingGravity.x
         const x = event.accelerationIncludingGravity.x;
         const y = event.accelerationIncludingGravity.y;
         const z = event.accelerationIncludingGravity.z;
