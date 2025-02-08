b0ee4b911d4693b854e4e7d879d2a5aa90a9c5ce
Anna <anna@akufen.ca>
2025-02-07 21:53:56 -0500

    control tree

--- src/Experience/Utils/DeviceOrientation.js
+++ src/Experience/Utils/DeviceOrientation.js
@@ -32,10 +32,10 @@ export default class DeviceOrientation extends EventEmitter
         // const x = event.accelerationIncludingGravity.x;
         // const y = event.accelerationIncludingGravity.y;
         // const z = event.accelerationIncludingGravity.z;
-        // this.experience.world.snow.dt = event.alpha * 0.001
-        this.experience.camera.instance.rotation.x = event.alpha * (Math.PI / 180)
-        this.experience.camera.instance.rotation.y = event.beta * (Math.PI / 180)
-        this.experience.camera.instance.rotation.z = event.gamma * (Math.PI / 180)
+        this.experience.world.tree.position.y += event.alpha * 0.1
+        // this.experience.camera.instance.rotation.x = event.alpha * (Math.PI / 180)
+        // this.experience.camera.instance.rotation.y = event.beta * (Math.PI / 180)
+        // this.experience.camera.instance.rotation.z = event.gamma * (Math.PI / 180)
       
         // Do something awesome.
     }
