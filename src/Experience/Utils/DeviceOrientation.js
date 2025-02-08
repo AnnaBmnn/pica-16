8632c03de38f2cfef3d12b88b75b6f3cdc5d3e86
Anna <anna@akufen.ca>
2025-02-07 19:19:26 -0500

    switc to device orientation

--- src/Experience/Utils/DeviceOrientation.js
+++ src/Experience/Utils/DeviceOrientation.js
@@ -7,26 +7,26 @@ export default class DeviceOrientation extends EventEmitter
         super()
 
         console.log('HEY')
-        if (typeof DeviceMotionEvent.requestPermission === 'function') {
+        if (typeof DeviceOrientationEvent.requestPermission === 'function') {
 
-            DeviceMotionEvent.requestPermission()
+            DeviceOrientationEvent.requestPermission()
             .then(response => {
                 if (response == 'granted') {
-                    window.addEventListener("devicemotion", this.handleMotionEvent, true);
+                    window.addEventListener("deviceorientation", this.handleMotionEvent, true);
 
                 }
             })
             .catch(console.error)
         } else {
-            window.addEventListener("devicemotion", this.handleMotionEvent, true);
+            window.addEventListener("deviceorientation", this.handleMotionEvent, true);
         }
     }
     handleMotionEvent(event) {
         console.log(event)
-        document.querySelector('.js-title').innerHTML = event.accelerationIncludingGravity.x
-        const x = event.accelerationIncludingGravity.x;
-        const y = event.accelerationIncludingGravity.y;
-        const z = event.accelerationIncludingGravity.z;
+        document.querySelector('.js-title').innerHTML = event.alpha + ' ' + event.beta + ' ' + event.gamma
+        // const x = event.accelerationIncludingGravity.x;
+        // const y = event.accelerationIncludingGravity.y;
+        // const z = event.accelerationIncludingGravity.z;
       
         // Do something awesome.
     }
