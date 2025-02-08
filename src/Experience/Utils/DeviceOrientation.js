58781e91ae6937d2ec6eaf9c1591070ca89caab3
Anna <anna@akufen.ca>
2025-02-07 22:13:08 -0500

    point light

--- src/Experience/Utils/DeviceOrientation.js
+++ src/Experience/Utils/DeviceOrientation.js
@@ -26,13 +26,21 @@ export default class DeviceOrientation extends EventEmitter
         })
 
     }
+    mapNumRange(num, inMin, inMax, outMin, outMax)
+    {
+        return ((num - inMin) * (outMax - outMin)) / (inMax - inMin) + outMin;
+
+    }
     handleMotionEvent(event) {
         console.log(event)
-        document.querySelector('.debug').innerHTML = event.alpha + ' ' + event.beta + ' ' + event.gamma + this.experience.camera.instance.rotation.x
+        const rangeNumber = this.mapNumRange(360 - event.alpha, 0, 360, -5, 5 )
+
+        this.experience.world.environment.pointLight.position.x += rangeNumber
+        document.querySelector('.debug').innerHTML = rangeNumber
         // const x = event.accelerationIncludingGravity.x;
         // const y = event.accelerationIncludingGravity.y;
         // const z = event.accelerationIncludingGravity.z;
-        this.experience.world.tree.tree.children[0].material.emissiveIntesity = event.alpha * 0.1
+        // this.experience.world.tree.tree.children[0].material.emissiveIntesity = this.mapNumRange(360 - event.alpha, 0, 360, -5, 5 )
         // this.experience.camera.instance.rotation.x = event.alpha * (Math.PI / 180)
         // this.experience.camera.instance.rotation.y = event.beta * (Math.PI / 180)
         // this.experience.camera.instance.rotation.z = event.gamma * (Math.PI / 180)
